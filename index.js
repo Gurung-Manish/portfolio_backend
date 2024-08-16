19e1fd828445c57256ed2866ecbdc5ed3e6adda1
Gurung-Manish <manishgrg496@gmail.com>
2024-08-16 15:37:05 +0100

    connecting mongodb

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("server working on port 3000");
+app.get("/", (req, res) => {
+  res.send("This is default getaway new");
 });
 
-app.get("/", (req, res) => {
-  res.send("This is default getaway");
-});
+mongoose
+  .connect(
+    "mongodb+srv://manish:Thisismg007@portfoliodb.gzq3b.mongodb.net/PortfolioDB?retryWrites=true&w=majority&appName=PortfolioDB"
+  )
+  .then(() => {
+    console.log("Connected! to database !!!");
+    app.listen(3000, () => {
+      console.log("server working on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Failed to connect to db!!");
+  });
