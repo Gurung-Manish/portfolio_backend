43ce2e5edc85627f1236ef046bed661faacecd46
Gurung-Manish <manishgrg496@gmail.com>
2024-08-16 18:36:36 +0100

    all https request of experience model done

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const ExperienceModel = require("./models/experience.model.js");
+const e = require("express");
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("This is default getaway new");
@@ -14,6 +16,54 @@ app.post("/experience", async (req, res) => {
   try {
     const expereince = await ExperienceModel.create(req.body);
     res.status(200).json(expereince);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/experience", async (req, res) => {
+  try {
+    const experiences = await ExperienceModel.find({});
+    res.status(200).json(experiences);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/experience/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const experience = await ExperienceModel.findById(id);
+    res.status(200).json(experience);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/experience/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const experience = await ExperienceModel.findByIdAndUpdate(id, req.body);
+
+    if (!experience) {
+      return res.status(404).json({ message: "Experience Not Found" });
+    }
+
+    const updatedExperience = await ExperienceModel.findById(id);
+    res.status(200).json(updatedExperience);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/experience/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const experience = await ExperienceModel.findByIdAndDelete(id);
+    if (!experience) {
+      return res.status(404).json({ message: "Data Not Found" });
+    }
+    res.status(200).json({ message: "Experience Deleted" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
