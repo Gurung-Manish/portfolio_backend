b0dcafc7d9d9a654792f0d647e9601c4c72fd3ed
Gurung-Manish <manishgrg496@gmail.com>
2024-08-17 20:34:09 +0100

    router created

--- routes/education.route.js
+++ routes/education.route.js
@@ -1,3 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const {} = require("");
+const {
+  getEducation,
+  getEducations,
+  addEducation,
+  updateEducation,
+  deleteEducation,
+} = require("../controller/education.controller.js");
+
+router.get("/", getEducations);
+
+router.get("/:id", getEducation);
+
+router.post("/", addEducation);
+
+router.put("/", updateEducation);
+
+router.delete("/:id", deleteEducation);
+
+module.export = router;
