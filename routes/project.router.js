a0c659c7dfcfcab83b1b9ba5266cc2c6d844f64a
Gurung-Manish <manishgrg496@gmail.com>
2024-08-20 17:47:58 +0100

    project router done

---
+++ routes/project.router.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getProjects,
+  getProject,
+  addProject,
+  updateProject,
+  deleteProject,
+} = require("../controller/project.controller.js");
+
+router.get("/", getProjects);
+
+router.get("/:id", getProject);
+
+router.post("/", addProject);
+
+router.put("/", updateProject);
+
+router.delete("/:id", deleteProject);
+
+module.exports = router;
